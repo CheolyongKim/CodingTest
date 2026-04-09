04ec08b61d1d4cdf16cf32b2362c4f40e34a1391
CheolyongKim <chdragon01@hanyang.ac.kr>
2026-04-09 17:44:36 +0900

    [Gold V] Title: 수 고르기, Time: 1256 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -7,30 +7,32 @@ using namespace std;
 int N, M, a;
 vector<int> A;
 
-int find_first(int i, int r) {
-	while (i<A.size() && A[i] < r) { i++; }
-	if (i > A.size() - 1) return -1;
+int find_first(int i, int right) {
+	while (i<A.size() && A[i] < right) { i++; }
 	return i;
 }
 
 int main() {
+	// input and sort
 	cin >> N >> M;
 	for (; N > 0; N--) {
 		cin >> a;
 		A.push_back(a);
 	}
 	sort(A.begin(), A.end());
-	int min = A.back() - A[0];
 
-	int l = A[0], r;
-	for (int i = 0; i < A.size() - 1; l = A[++i]) {
-		r = l + M;
-		int f = find_first(i, r);
-		if (f == -1) break;
-		if (A[f] - l < min) min = A[f] - l;
+	// find minimum
+	int min = A.back() - A[0];	// maximum diff
+
+	int left = A[0], right;
+	for (int i = 0; i < A.size() - 1; left = A[++i]) {
+		right = left + M;
+		int f = find_first(i, right);
+		if (f == A.size()) break;
+		if (A[f] - left < min) min = A[f] - left;
 	}
 	int i = A.size() - 1;
-	while (A[i] > l) { i--; }
+	while (A[i] > left) { i--; }
 	if (A.back() - A[i]>=M && A.back()-A[i] < min) min = A.back() - A[i];
 
 	cout << min;
